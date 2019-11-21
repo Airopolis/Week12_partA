ff358c9340f8a1e66221fa3e4b16ef560282d58f
Airopolis <57768324+Airopolis@users.noreply.github.com>
2019-11-21 11:30:41 -0700

    Add files via upload

---
+++ Week12-utility.py
@@ -0,0 +1,17 @@
+#Andrew Aire
+
+def PrintOutput(a):
+    print('OUTPUT', a)
+
+def LoadFile(a):
+    f = open(a,'r')
+    for i in len(f):
+        newfile = newfile.append[i]
+    print('OUTPUT',newfile)
+
+def UpdateString(a, b, c):
+    a[c] = b
+    print('OUTPUT',a)
+
+def FindWordCount(a, b):
+    f = open(a,'r')
